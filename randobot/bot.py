485397f8890b7b7f1c60544f97f47bf62efc3641
Damian Posener <750763+deains@users.noreply.github.com>
2020-02-09 11:49:08 +0000

    Remove debug host

--- randobot/bot.py
+++ randobot/bot.py
@@ -8,9 +8,6 @@ class RandoBot(Bot):
     """
     RandoBot base class. Uses the
     """
-    racetime_host = 'localhost:8000'
-    racetime_secure = False
-
     def __init__(self, ootr_api_key, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.zsr = ZSR(ootr_api_key)
