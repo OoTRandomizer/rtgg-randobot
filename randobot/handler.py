a822dba1cb9baf43f21f69751b86430b0ff61fc9
Damian Posener <750763+deains@users.noreply.github.com>
2022-02-17 09:40:24 +0000

    Update to v2, add hashes, and stuff

--- randobot/handler.py
+++ randobot/handler.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 from racetime_bot import RaceHandler, monitor_cmd, can_moderate, can_monitor
 
 
@@ -5,14 +6,13 @@ class RandoHandler(RaceHandler):
     """
     RandoBot race handler. Generates seeds, presets, and frustration.
     """
+    seed_url = 'https://ootrandomizer.com/seed/get?id=%s'
     stop_at = ['cancelled', 'finished']
+    max_status_checks = 50
 
     def __init__(self, zsr, **kwargs):
         super().__init__(**kwargs)
-
         self.zsr = zsr
-        self.presets = zsr.load_presets()
-        self.seed_rolled = False
 
     async def begin(self):
         """
@@ -134,7 +134,7 @@ class RandoHandler(RaceHandler):
                 % {'reply_to': reply_to or 'friend'}
             )
             return
-        if self.state.get('seed_rolled') and not can_moderate(message):
+        if self.state.get('seed_id') and not can_moderate(message):
             await self.send_message(
                 'Well excuuuuuse me princess, but I already rolled a seed. '
                 'Don\'t get greedy!'
@@ -151,7 +151,7 @@ class RandoHandler(RaceHandler):
         """
         Generate a seed and send it to the race room.
         """
-        if preset not in self.presets:
+        if preset not in self.zsr.presets:
             await self.send_message(
                 'Sorry %(reply_to)s, I don\'t recognise that preset. Use '
                 '!presets to see what is available.'
@@ -159,23 +159,49 @@ class RandoHandler(RaceHandler):
             )
             return
 
-        seed_uri = self.zsr.roll_seed(self.presets[preset], encrypt)
+        seed_id, seed_uri = self.zsr.roll_seed(preset, encrypt)
 
         await self.send_message(
             '%(reply_to)s, here is your seed: %(seed_uri)s'
             % {'reply_to': reply_to or 'Okay', 'seed_uri': seed_uri}
         )
-        await self.set_raceinfo(seed_uri)
+        await self.set_bot_raceinfo(seed_uri)
 
-        self.state['seed_rolled'] = True
+        self.state['seed_id'] = seed_id
+        self.state['status_checks'] = 0
+
+        await self.check_seed_status()
+
+    async def check_seed_status(self):
+        await sleep(1)
+        status = self.zsr.get_status(self.state['seed_id'])
+        if status == 0:
+            self.state['status_checks'] += 1
+            if self.state['status_checks'] < self.max_status_checks:
+                await self.check_seed_status()
+        elif status == 1:
+            await self.load_seed_hash()
+        elif status >= 2:
+            self.state['seed_id'] = None
+            await self.send_message(
+                'Sorry, but it looks like the seed failed to generate. Use '
+                '!seed to try again.'
+            )
+
+    async def load_seed_hash(self):
+        seed_hash = self.zsr.get_hash(self.state['seed_id'])
+        await self.set_bot_raceinfo('%(seed_hash)s\n%(seed_url)s' % {
+            'seed_hash': seed_hash,
+            'seed_url': self.seed_url % self.state['seed_id'],
+        })
 
     async def send_presets(self):
         """
         Send a list of known presets to the race room.
         """
         await self.send_message('Available presets:')
-        for name, full_name in self.presets.items():
-            await self.send_message(f'{name} – {full_name}')
+        for name, preset in self.zsr.presets.items():
+            await self.send_message('%s – %s' % (name, preset['full_name']))
 
     def _race_in_progress(self):
         return self.data.get('status').get('value') in ('pending', 'in_progress')
--- randobot/zsr.py
+++ randobot/zsr.py
@@ -8,31 +8,98 @@ class ZSR:
     Class for interacting with ootrandomizer.com to generate seeds, and
     zeldaspeedruns.com to get available presets.
     """
-    seed_public = 'https://ootrandomizer.com/seed/get?id=%(seedID)s'
-    seed_endpoint = 'https://ootrandomizer.com/api/seed/preset'
+    seed_public = 'https://ootrandomizer.com/seed/get?id=%(id)s'
+    seed_endpoint = 'https://ootrandomizer.com/api/v2/seed/create'
+    status_endpoint = 'https://ootrandomizer.com/api/v2/seed/status'
+    details_endpoint = 'https://ootrandomizer.com/api/v2/seed/details'
     preset_endpoint = 'https://www.zeldaspeedruns.com/assets/ootr_presets.json'
+    settings_endpoint = 'https://raw.githubusercontent.com/TestRunnerSRL/OoT-Randomizer/release/data/presets_default.json'
+
+    hash_map = {
+        'Beans': 'HashBeans',
+        'Big Magic': 'HashBigMagic',
+        'Bombchu': 'HashBombchu',
+        'Boomerang': 'HashBoomerang',
+        'Boss Key': 'HashBossKey',
+        'Bottled Fish': 'HashBottledFish',
+        'Bottled Milk': 'HashBottledMilk',
+        'Bow': 'HashBow',
+        'Compass': 'HashCompass',
+        'Cucco': 'HashCucco',
+        'Deku Nut': 'HashDekuNut',
+        'Deku Stick': 'HashDekuStick',
+        'Fairy Ocarina': 'HashFairyOcarina',
+        'Frog': 'HashFrog',
+        'Gold Scale': 'HashGoldScale',
+        'Heart Container': 'HashHeart',
+        'Hover Boots': 'HashHoverBoots',
+        'Kokiri Tunic': 'HashKokiriTunic',
+        'Lens of Truth': 'HashLensOfTruth',
+        'Longshot': 'HashLongshot',
+        'Map': 'HashMap',
+        'Mask of Truth': 'HashMaskOfTruth',
+        'Master Sword': 'HashMasterSword',
+        'Megaton Hammer': 'HashHammer',
+        'Mirror Shield': 'HashMirrorShield',
+        'Mushroom': 'HashMushroom',
+        'Saw': 'HashSaw',
+        'Silver Gauntlets': 'HashSilvers',
+        'Skull Token': 'HashSkullToken',
+        'Slingshot': 'HashSlingshot',
+        'SOLD OUT': 'HashSoldOut',
+        'Stone of Agony': 'HashStoneOfAgony',
+    }
 
     def __init__(self, ootr_api_key):
         self.ootr_api_key = ootr_api_key
+        self.presets = self.load_presets()
 
     def load_presets(self):
         """
         Load and return available seed presets.
         """
-        resp = requests.get(self.preset_endpoint)
-        data = json.loads(resp.content)
+        presets = requests.get(self.preset_endpoint).json()
+        settings = requests.get(self.settings_endpoint).json()
         return {
-            key: value['fullName']
-            for key, value in data.items()
+            key: {
+                'full_name': value['fullName'],
+                'settings': settings.get(value['fullName']),
+            }
+            for key, value in presets.items()
+            if value['fullName'] in settings
         }
 
     def roll_seed(self, preset, encrypt):
         """
         Generate a seed and return its public URL.
         """
-        resp = requests.post(self.seed_endpoint, preset, params={
+        req_body = json.dumps(self.presets[preset]['settings'])
+        params = {
             'key': self.ootr_api_key,
-            'encrypt': 'true' if encrypt else 'false',
-        }, headers={'Content-Type': 'text/plain'})
-        data = json.loads(resp.content)
-        return self.seed_public % data
+        }
+        if encrypt:
+            params['encrypt'] = 'true'
+        data = requests.post(self.seed_endpoint, req_body, params=params,
+                             headers={'Content-Type': 'application/json'}).json()
+        return data['id'], self.seed_public % data
+
+    def get_status(self, seed_id):
+        data = requests.get(self.status_endpoint, params={
+            'id': seed_id,
+            'key': self.ootr_api_key,
+        }).json()
+        return data['status']
+
+    def get_hash(self, seed_id):
+        data = requests.get(self.details_endpoint, params={
+            'id': seed_id,
+            'key': self.ootr_api_key,
+        }).json()
+        try:
+            settings = json.loads(data.get('settingsLog'))
+        except ValueError:
+            return None
+        return ' '.join(
+            self.hash_map.get(item, item)
+            for item in settings['file_hash']
+        )
