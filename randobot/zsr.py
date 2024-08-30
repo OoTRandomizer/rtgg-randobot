2b313803e3b9eff9edddc469af78be283695e6c1
Damian Posener <750763+deains@users.noreply.github.com>
2024-08-30 19:00:54 +0100

    Catch TypeError if JSON response is not valid

--- randobot/zsr.py
+++ randobot/zsr.py
@@ -205,7 +205,7 @@ class ZSR:
                     self.notes_map.get(item, item)
                     for item in password_notes
                 )
-            except (ValueError, requests.RequestException):
+            except (TypeError, ValueError, requests.RequestException):
                 if attempt < retries - 1:
                     time.sleep(delay)
                 else:
